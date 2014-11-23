fd0067d152aba723f7c1b5fc6f43bb9e8081da17
Michal Smialko <michal.smialko@gmail.com>
2014-11-23 11:37:21 +0100

    Added new empty Xcode project

---
+++ KrakSpotApp/KrakSpotApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KrakSpotApp
+//
+//  Created by Michal Smialko on 11/23/14.
+//  Copyright (c) 2014 Prismake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KrakSpotApp/KrakSpotApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KrakSpotApp
+//
+//  Created by Michal Smialko on 11/23/14.
+//  Copyright (c) 2014 Prismake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
